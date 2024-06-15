f76c1e55cd67121414ba4e72428c7661cf1a2887
krishandeep17 <krishandeep17@gmail.com>
2024-06-15 10:05:11 +0530

    feat: add date-fns package to format relative date

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,4 +1,11 @@
+import { formatDistanceToNow, parseISO } from "date-fns";
+
 export const formatCurrency = (value) =>
   new Intl.NumberFormat("en", { style: "currency", currency: "USD" }).format(
     value
   );
+
+export function formatRelativeDate(dateString) {
+  const date = parseISO(dateString);
+  return formatDistanceToNow(date, { addSuffix: true }).replace("about ", "");
+}
