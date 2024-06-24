df858f1913228b14cd28f389834b0e1e93576632
krishandeep17 <krishandeep17@gmail.com>
2024-06-24 12:04:54 +0530

    feat: add default styles and theme

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,28 +1,7 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#131118",
-    },
-  },
-
-  typography: {
-    fontFamily: ["Jost", "sans-serif"].join(","),
-  },
-
-  breakpoints: {
-    values: {
-      xxs: 0,
-      xs: 500,
-      sm: 600,
-      md: 900,
-      lg: 1200,
-      xl: 1536,
-    },
-  },
-
+let theme = createTheme({
   components: {
     MuiButton: {
       defaultProps: {
@@ -31,24 +10,65 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           borderRadius: "10px",
-          padding: "10px 20px",
+          padding: "10px 24px",
           textTransform: "none",
+          fontSize: "1rem",
+          fontWeight: "normal",
         },
       },
     },
-    MuiLink: {
-      styleOverrides: {
-        root: {
-          textDecoration: "none",
-        },
-      },
-    },
+
     MuiStack: {
       defaultProps: {
         useFlexGap: true,
       },
     },
   },
+
+  palette: {
+    mode: "light",
+
+    primary: {
+      main: "#131118",
+    },
+
+    brand: {
+      gray_50: "rgb(255, 255, 255)",
+      gray_100: "rgb(243, 243, 243)",
+      gray_200: "rgb(237, 236, 238)",
+      gray_500: "rgb(164, 161, 170)",
+
+      green: "rgb(60, 209, 57)",
+      red: "rgb(255, 111, 118)",
+    },
+  },
+
+  typography: {
+    fontFamily: '"Jost", sans-serif',
+
+    h2: {
+      fontWeight: "bold",
+    },
+
+    h3: {
+      fontSize: "2rem",
+    },
+
+    h4: {
+      fontWeight: "bold",
+    },
+
+    h5: {
+      fontSize: "1.875rem",
+      fontWeight: "bold",
+    },
+
+    subtitle1: {
+      fontSize: "1.625rem",
+    },
+  },
 });
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
