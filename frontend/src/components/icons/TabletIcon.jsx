3bc21153862e3eb0eab901f020b3f61b66ef4a19
krishandeep17 <krishandeep17@gmail.com>
2024-07-12 09:59:10 +0530

    feat: add icons for categories

---
+++ frontend/src/components/icons/TabletIcon.jsx
@@ -0,0 +1,22 @@
+import { SvgIcon } from "@mui/material";
+
+export default function TabletIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <rect width="16" height="20" x="4" y="2" rx="2" ry="2" />
+        <line x1="12" x2="12.01" y1="18" y2="18" />
+      </svg>
+    </SvgIcon>
+  );
+}
---
+++ frontend/src/components/icons/TelevisionIcon.jsx
@@ -0,0 +1,22 @@
+import { SvgIcon } from "@mui/material";
+
+export default function TelevisionIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <rect width="20" height="15" x="2" y="7" rx="2" ry="2" />
+        <polyline points="17 2 12 7 7 2" />
+      </svg>
+    </SvgIcon>
+  );
+}
