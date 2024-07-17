c4180d66356839099d15a2bbfc2615e003f0a580
krishandeep17 <krishandeep17@gmail.com>
2024-07-17 11:05:46 +0530

    feat: get products from database

---
+++ backend/controllers/productController.js
@@ -0,0 +1,31 @@
+import Product from "../models/productModel.js";
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: `Server Error: ${error}` });
+  }
+};
+
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+export const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: `Server Error: ${error}` });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 import express from "express";
 
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const port = process.env.PORT || 3000;
 
-// Connect to database
-connectDB();
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.send("Hello World!");
-});
+// Routes
+app.use("/api/products", productRoutes);
 
 app.listen(port, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
