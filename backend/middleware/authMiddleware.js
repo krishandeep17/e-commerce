c71e9e34139b96d2a77b2201e0e35d13eceda1fb
krishandeep17 <krishandeep17@gmail.com>
2024-08-03 13:00:41 +0530

    feat (auth) : create authorized routes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -26,3 +26,15 @@ export const protect = asyncHandler(async (req, res, next) => {
 
   next();
 });
+
+export const authorize =
+  (...roles) =>
+  (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action.", 403)
+      );
+    }
+
+    next();
+  };
